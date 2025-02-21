83a60ee0234e49c5db37fb3154cb0e51ee4ca027
Ajohno <44455099+Ajohno@users.noreply.github.com>
2025-02-21 14:29:23 -0500

    -Added a register button on the login page
    - Created a login page for users to make accounts

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -28,7 +28,7 @@ const login = () => {
       //absolute inset-0 bg-black opacity-50 
       <div className="flex items-center justify-center min-h-screen bg-cover bg-center bg-[url('/freedomtrail_medallion.jpg')] " > 
       <div className="bg-[#DCEDFF] bg-opacity-20 backdrop-blur-lg p-8 rounded-3xl w-96 ">
-        <h2 className="text-4xl font-bold text-center text-blue-900 mb-6 font-garamond">Sign in</h2>
+        <h2 className="text-4xl font-bold text-center text-blue-900 mb-6 font-garamond">Register</h2>
         
         <form onSubmit={handleLogin} className="flex flex-col">
           <label className="text-blue-900 font-garamond text-2xl font-extrabold mb-2">Email:</label>
@@ -41,7 +41,7 @@ const login = () => {
             required
           />
 
-          <label className="text-blue-900 font-garamond text-2xl font-extrabold mb-2">Password:</label>
+          <label className="text-blue-900 text-2xl font-garamond font-extrabold mb-2">Password:</label>
           <input
             type="password"
             placeholder=""
@@ -51,29 +51,12 @@ const login = () => {
             required
           />
 
-          {/* Google Sign-In Button */}
-          <button className="flex items-center justify-center bg-white hover:bg-slate-300 text-black p-2 rounded-full shadow-md mb-4">
-            <img src="google.png" alt="Google Logo" className="w-6 h-6 mr-2" />
-            Sign in with Google
-          </button>
-
           {/* Login Button */}
-          <button type="submit" className="bg-blue-900 text-white p-3 rounded-full text-2xl font-semibold font-garamond hover:bg-blue-800 transition">
+          <button type="submit" className="bg-blue-900 text-2xl text-white p-3 rounded-full  font-semibold font-garamond hover:bg-blue-800 transition">
             Start your tour
           </button>
         </form>
 
-        {/* Forgot Password */}
-        <p className="text-center text-blue-900 font-garamond font-semibold mt-4">
-          <a href="#" className="underline">Forgot your password?</a>
-        </p>
-
-        {/* Navigate to register page */}
-        <button 
-        onClick={handleRegisterNavigation} 
-        className="ml-24 h-7 w-28 mt-4  text-blue-900 rounded-full text-lg font-semibold font-garamond bg-[#dcedff] hover:bg-[#b7d3f0] transition">
-          Or Register
-        </button>
       </div>
     </div>
     );
