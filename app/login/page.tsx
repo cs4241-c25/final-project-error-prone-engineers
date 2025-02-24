99f453d6596051c1353ae4d3dc7aad293c15a204
Ajohno <44455099+Ajohno@users.noreply.github.com>
2025-02-24 12:19:55 -0500

    Updated CSS for the login page and register page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -17,7 +17,7 @@ const login = () => {
         router.push('/');
     }
 
-    // Handle the login
+    // Handle the navigation to register page
     async function handleRegisterNavigation(e: React.FormEvent){
       e.preventDefault();
       console.log('Navigating to registration form.');
@@ -27,11 +27,11 @@ const login = () => {
     return (
       //absolute inset-0 bg-black opacity-50 
       <div className="flex items-center justify-center min-h-screen bg-cover bg-center bg-[url('/freedomtrail_medallion.jpg')] " > 
-      <div className="bg-[#DCEDFF] bg-opacity-20 backdrop-blur-lg p-8 rounded-3xl w-96 ">
-        <h2 className="text-4xl font-bold text-center text-blue-900 mb-6 font-garamond">Sign in</h2>
+      <div className="bg-[#DCEDFF] bg-opacity-20 backdrop-blur-lg p-8 rounded-3xl w-2/5 ">
+        <h2 className="text-6xl font-bold text-center text-blue-900 mb-6 font-garamond">Sign in</h2>
         
         <form onSubmit={handleLogin} className="flex flex-col">
-          <label className="text-blue-900 font-garamond text-2xl font-extrabold mb-2">Email:</label>
+          <label className="text-blue-900 font-garamond text-4xl font-extrabold mb-2">Email:</label>
           <input
             type="email"
             placeholder=""
@@ -41,7 +41,7 @@ const login = () => {
             required
           />
 
-          <label className="text-blue-900 font-garamond text-2xl font-extrabold mb-2">Password:</label>
+          <label className="text-blue-900 font-garamond text-4xl font-extrabold mb-2">Password:</label>
           <input
             type="password"
             placeholder=""
@@ -51,27 +51,31 @@ const login = () => {
             required
           />
 
+          {/* Login Button */}
+          <button type="submit" className="bg-blue-900 mb-4 text-white p-2 rounded-full text-2xl font-semibold font-garamond hover:bg-blue-800 transition">
+            Start your tour
+          </button>
+
+          <hr className="mb-4"></hr>
+
           {/* Google Sign-In Button */}
-          <button className="flex items-center justify-center bg-white hover:bg-slate-300 text-black p-2 rounded-full shadow-md mb-4">
+          <button className="flex items-center justify-center bg-white hover:bg-slate-300 text-black p-2 rounded-full shadow-md ">
             <img src="google.png" alt="Google Logo" className="w-6 h-6 mr-2" />
             Sign in with Google
           </button>
 
-          {/* Login Button */}
-          <button type="submit" className="bg-blue-900 text-white p-3 rounded-full text-2xl font-semibold font-garamond hover:bg-blue-800 transition">
-            Start your tour
-          </button>
+          
         </form>
 
         {/* Forgot Password */}
-        <p className="text-center text-blue-900 font-garamond font-semibold mt-4">
+        <p className="text-center text-blue-900 font-garamond text-xl font-semibold mt-4">
           <a href="#" className="underline">Forgot your password?</a>
         </p>
 
         {/* Navigate to register page */}
         <button 
         onClick={handleRegisterNavigation} 
-        className="ml-24 h-7 w-28 mt-4  text-blue-900 rounded-full text-lg font-semibold font-garamond bg-[#dcedff] hover:bg-[#b7d3f0] transition">
+        className="ml-56 h-7 w-28 mt-4  text-blue-900 rounded-full text-lg font-semibold font-garamond bg-[#dcedff] hover:bg-[#b7d3f0] transition">
           Or Register
         </button>
       </div>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -17,44 +17,55 @@ const login = () => {
         router.push('/');
     }
 
-    // Handle the login
-    async function handleRegisterNavigation(e: React.FormEvent){
-      e.preventDefault();
-      console.log('Navigating to registration form.');
-      router.push('/register');
-  }
+    // Handle the navigation to login page
+    async function handleLoginNavigation(e: React.FormEvent){
+        e.preventDefault();
+        console.log('Navigating to login form.');
+        router.push('/login');
+    }
 
     return (
       //absolute inset-0 bg-black opacity-50 
       <div className="flex items-center justify-center min-h-screen bg-cover bg-center bg-[url('/freedomtrail_medallion.jpg')] " > 
-      <div className="bg-[#DCEDFF] bg-opacity-20 backdrop-blur-lg p-8 rounded-3xl w-96 ">
-        <h2 className="text-4xl font-bold text-center text-blue-900 mb-6 font-garamond">Register</h2>
+      <div className="bg-[#DCEDFF] bg-opacity-20 backdrop-blur-lg p-8 rounded-3xl w-2/5 ">
+        <h2 className="text-6xl font-bold text-center text-blue-900 mb-6 font-garamond">Register</h2>
         
         <form onSubmit={handleLogin} className="flex flex-col">
-          <label className="text-blue-900 font-garamond text-2xl font-extrabold mb-2">Email:</label>
-          <input
-            type="email"
-            placeholder=""
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="w-full p-3 rounded-full bg-[#2F1000] bg-opacity-50 text-white focus:outline-none mb-4"
-            required
-          />
+            <label className="text-blue-900 font-garamond text-4xl font-extrabold mb-2">Email:</label>
+            <input
+                type="email"
+                placeholder=""
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="w-full p-3 rounded-full bg-[#2F1000] bg-opacity-50 text-white focus:outline-none mb-4"
+                required
+            />
 
-          <label className="text-blue-900 text-2xl font-garamond font-extrabold mb-2">Password:</label>
-          <input
-            type="password"
-            placeholder=""
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full p-3 rounded-full bg-[#2F1000] bg-opacity-50 text-white focus:outline-none mb-6"
-            required
-          />
+            <label className="text-blue-900 text-4xl font-garamond font-extrabold mb-2">Password:</label>
+            <input
+                type="password"
+                placeholder=""
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full p-3 rounded-full bg-[#2F1000] bg-opacity-50 text-white focus:outline-none mb-6"
+                required
+            />
 
-          {/* Login Button */}
-          <button type="submit" className="bg-blue-900 text-2xl text-white p-3 rounded-full  font-semibold font-garamond hover:bg-blue-800 transition">
-            Start your tour
-          </button>
+            
+
+            {/* Register Button */}
+            <button type="submit" className="bg-blue-900 text-2xl mb-4 text-white p-2 rounded-full  font-semibold font-garamond hover:bg-blue-800 transition">
+                Start your tour
+            </button>
+
+            <hr className="mb-4"></hr>
+
+            {/* Navigate to login page */}
+            <button 
+            onClick={handleLoginNavigation} 
+            className="ml-56 h-7 w-28 mt-2  text-blue-900 rounded-full text-lg font-semibold font-garamond bg-[#dcedff] hover:bg-[#b7d3f0] transition">
+            Or Login
+            </button>
         </form>
 
       </div>
