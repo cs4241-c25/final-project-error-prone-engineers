9eb7005183f92fdf7729c7ee2cdd14c0c9283c64
keiraSchoolcraft <keira.schoolcraft@icloud.com>
2025-02-26 17:22:59 -0500

    bathroom icon on map

--- components/FreedomMap.tsx
+++ components/FreedomMap.tsx
@@ -11,6 +11,22 @@ L.Icon.Default.mergeOptions({
   shadowUrl: "/_next/static/images/marker-shadow.png",
 });
 
+const restroomIcon = L.divIcon({
+  className: "custom-restroom-icon",
+  html: `
+    <div style="width: 32px; height: 32px;">
+      <svg width="37" height="37" viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M11.5626 10.7917C13.2655 10.7917 14.6459 9.41121 14.6459 7.70833C14.6459 6.00546 13.2655 4.625 11.5626 4.625C9.8597 4.625 8.47925 6.00546 8.47925 7.70833C8.47925 9.41121 9.8597 10.7917 11.5626 10.7917Z" fill="#0A2463" stroke="#0A2463" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M25.4376 10.7917C27.1405 10.7917 28.5209 9.41121 28.5209 7.70833C28.5209 6.00546 27.1405 4.625 25.4376 4.625C23.7347 4.625 22.3542 6.00546 22.3542 7.70833C22.3542 9.41121 23.7347 10.7917 25.4376 10.7917Z" fill="#0A2463" stroke="#0A2463" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M7.70837 15.4167H15.4167L13.875 32.375H9.25004L7.70837 15.4167ZM21.5834 15.4167H29.2917L30.8334 23.8958H28.5209L27.75 32.375H23.125L22.3542 23.8958H20.0417L21.5834 15.4167Z" fill="#0A2463" stroke="#0A2463" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+    </div>`,
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32],
+});
+
+
 interface MapProps {
   geoJsonData: FeatureCollection | null;
   geoJsonDataRestrooms: FeatureCollection | null;
@@ -64,7 +80,11 @@ const FreedomMap: React.FC<MapProps> = ({ geoJsonData, geoJsonDataRestrooms }) =
         color: "#0A2463",
         weight: 4,
       }),
-      pointToLayer: (feature, latlng) => L.marker(latlng),
+      pointToLayer: (feature, latlng) => {
+        return L.marker(latlng, { icon: restroomIcon }).bindPopup(
+          feature.properties?.name || "Restroom"
+        );
+      },
     }).addTo(map);
 
     // Fit map to bounds
