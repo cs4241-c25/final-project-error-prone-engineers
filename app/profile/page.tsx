15d3d49e69801723d5becf01fa50c7d003ca85f7
Ajohno <44455099+Ajohno@users.noreply.github.com>
2025-02-27 15:33:26 -0500

    LoginButton.tsx checks if there is an active session (a user logged in) if there is someone logged in then the login button takes the user to the profile page if there isnt someone logged in then it takes them to the sign in page.

---
+++ app/profile/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from 'react';
+
+const ProfilePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Profile Page</h1>
+            <p>Welcome to your profile page!</p>
+        </div>
+    );
+};
+
+export default ProfilePage;
