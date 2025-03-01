bb0740649eefb83552a0c1f77b7f8be31cf486dd
Andy <aamesa03@gmail.com>
2025-03-01 11:49:24 -0500

    fized multiple connections

--- lib/database.tsx
+++ lib/database.tsx
@@ -1,27 +1,39 @@
-import { MongoClient, Db, Collection } from "mongodb";
+import { MongoClient, Db } from "mongodb";
 
 const MONGO_URI = process.env.MONGO_URI as string;
 const DB_NAME = "freedom-trail";
 
 let cachedClient: MongoClient | null = null;
 let cachedDb: Db | null = null;
+let cachedPromise: Promise<Db> | null = null;
 
-
-export async function connectDB(collectionName: string){
-    if (!cachedClient || !cachedDb) {
-        cachedClient = new MongoClient(MONGO_URI);
-        await cachedClient.connect();
-        cachedDb = cachedClient.db(DB_NAME);
+export async function connectDB(collectionName: string) {
+    if (cachedDb) {
+        return cachedDb.collection(collectionName);
     }
 
+    if (!cachedPromise) {
+        cachedPromise = (async () => {
+            const client = new MongoClient(MONGO_URI);
+            await client.connect();
+            cachedClient = client;
+            console.log("Connected to MongoDB");
+            cachedDb = client.db(DB_NAME);
+            return cachedDb;
+        })();
+    }
+
+    cachedDb = await cachedPromise;
     return cachedDb.collection(collectionName);
 }
 
 async function closeDB() {
     if (cachedClient) {
+        console.log("Database closed");
         await cachedClient.close();
         cachedClient = null;
         cachedDb = null;
+        cachedPromise = null;
     }
 }
 
