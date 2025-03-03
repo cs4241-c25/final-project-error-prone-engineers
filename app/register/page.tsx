35fd5c696de427c6865fdb33e23e3392966309fd
Ajohno <44455099+Ajohno@users.noreply.github.com>
2025-03-03 14:03:01 -0500

    Logout button on the profile page works

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,6 +2,7 @@
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import axios from "axios";
+import Banner from "@/components/Banner";
 
 const Register = () => {
     // Initialize the username and password state as blank
@@ -42,56 +43,58 @@ const Register = () => {
     }
 
     return (
-      //absolute inset-0 bg-black opacity-50 
-      <div className="flex items-center justify-center min-h-screen bg-cover bg-center bg-[url('/freedomtrail_medallion.jpg')] " > 
-      <div className="bg-[#DCEDFF] bg-opacity-20 backdrop-blur-lg p-8 rounded-3xl w-2/5 ">
-        <h2 className="text-6xl font-bold text-center text-blue-900 mb-6 font-garamond">Register</h2>
-        
-        <form onSubmit={handleRegister} className="flex flex-col">
-            <label className="text-blue-900 font-garamond text-4xl font-extrabold mb-2">Email:</label>
-            <input
-                type="email"
-                placeholder=""
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="w-full p-3 rounded-full bg-[#2F1000] bg-opacity-50 text-white focus:outline-none mb-4"
-                required
-            />
+        <div>
+            <Banner></Banner>
+        <div className="flex items-center justify-center min-h-screen bg-cover bg-center bg-[url('/freedomtrail_medallion.jpg')] " > 
+        <div className="bg-[#DCEDFF] mb-28 bg-opacity-20 backdrop-blur-lg p-8 rounded-3xl w-2/5 ">
+            <h2 className="text-6xl font-bold text-center text-blue-900 mb-6 font-garamond">Register</h2>
+            
+            <form onSubmit={handleRegister} className="flex flex-col">
+                <label className="text-blue-900 font-garamond text-4xl font-extrabold mb-2">Email:</label>
+                <input
+                    type="email"
+                    placeholder=""
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-full p-3 rounded-full bg-[#2F1000] bg-opacity-50 text-white focus:outline-none mb-4"
+                    required
+                />
 
-            <label className="text-blue-900 text-4xl font-garamond font-extrabold mb-2">Password:</label>
-            <input
-                type="password"
-                placeholder=""
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="w-full p-3 rounded-full bg-[#2F1000] bg-opacity-50 text-white focus:outline-none mb-6"
-                required
-            />
+                <label className="text-blue-900 text-4xl font-garamond font-extrabold mb-2">Password:</label>
+                <input
+                    type="password"
+                    placeholder=""
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="w-full p-3 rounded-full bg-[#2F1000] bg-opacity-50 text-white focus:outline-none mb-6"
+                    required
+                />
 
-            {/* Error Message */}
-            {error && (
-                <p className="text-red-500 font-semibold text-lg mb-4 text-center">
-                    {error}
-                </p>
-            )}
+                {/* Error Message */}
+                {error && (
+                    <p className="text-red-500 font-semibold text-lg mb-4 text-center">
+                        {error}
+                    </p>
+                )}
 
-            {/* Register Button */}
-            <button type="submit" className="bg-blue-900 text-2xl mb-4 text-white p-2 rounded-full  font-semibold font-garamond hover:bg-blue-800 transition">
-                Start your tour
-            </button>
+                {/* Register Button */}
+                <button type="submit" className="bg-blue-900 text-2xl mb-4 text-white p-2 rounded-full  font-semibold font-garamond hover:bg-blue-800 transition">
+                    Start your tour
+                </button>
 
-            <hr className="mb-4"></hr>
+                <hr className="mb-4"></hr>
 
-            {/* Navigate to login page */}
-            <button
-            onClick={handleLoginNavigation}
-            className="ml-56 h-7 w-28 mt-2  text-blue-900 rounded-full text-lg font-semibold font-garamond bg-[#dcedff] hover:bg-[#b7d3f0] transition">
-            Or Login
-            </button>
-        </form>
+                {/* Navigate to login page */}
+                <button
+                onClick={handleLoginNavigation}
+                className="ml-56 h-7 w-28 mt-2  text-blue-900 rounded-full text-lg font-semibold font-garamond bg-[#dcedff] hover:bg-[#b7d3f0] transition">
+                Or Login
+                </button>
+            </form>
 
-      </div>
-    </div>
+        </div>
+        </div>
+        </div>
     );
 };
 
