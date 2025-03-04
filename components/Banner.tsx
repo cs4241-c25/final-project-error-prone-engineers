886a4fd73d864aed9d1dca7dd7a96f954248386a
Andy <aamesa03@gmail.com>
2025-03-04 13:00:03 -0500

    fixing banner spacing

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,20 +4,28 @@ import LoginButton from '../components/LoginButton';
 import Link from 'next/link';
 
 const Banner = () => {
+    return (
+        <div className='flex items-center justify-between h-auto z-100 bg-[#D00000] p-2 w-full'>
+            {/* Left: Menu*/}
+            <div className="flex-shrink-0">
+                <Menu />
+            </div>
 
-  return (
-    <div className='flex flex-row h-1/7 z-100 bg-[#D00000] text-center items-center p-1 w-full'>
-        <div className="flex-shrink-0">
-          <Menu />
+            {/* Center: Title*/}
+            <div className="flex-grow text-center">
+                <Link href={'/'}>
+                    <h1 className='font-extrabold text-2xl md:text-6xl font-cinzel_decorative tracking-widest'>
+                        Boston Freedom Trail
+                    </h1>
+                </Link>
+            </div>
+
+            {/* Right: LoginButton */}
+            <div className='flex-shrink-0'>
+                <LoginButton />
+            </div>
         </div>
-        <Link href={'/'}>
-        <h1 className='font-extrabold text-3xl md:text-6xl ml-52 font-cinzel_decorative mx-auto tracking-widest'>Boston Freedom Trail</h1>
-        </Link>
-        <div className='flex-grow-0 ml-40'>
-          <LoginButton/>
-        </div>
-    </div>
-  );
+    );
 };
 
 export default Banner;
